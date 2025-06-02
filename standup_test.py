7703cd1d477c64ca949c0c2ef17f6c33aec523cc
csabahi <sabahi.camron@gmail.com>
2025-06-02 19:37:22 -0400

    initial push to branch with new xml

---
+++ standup_test.py
@@ -0,0 +1,15 @@
+import mujoco.viewer
+import mujoco
+import time
+import numpy as np
+
+model_path = "ankle_robot/scene.xml"
+
+model = mujoco.MjModel.from_xml_path(model_path)
+data = mujoco.MjData(model)
+
+with mujoco.viewer.launch_passive(model, data) as viewer:
+    while viewer.is_running():
+        mujoco.mj_step(model, data)
+        viewer.sync()
+        time.sleep(0.01)
